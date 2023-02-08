c0ccf65989dd70eddb54cdebaa50dba176d2b70b
hanifomarr <57039713+hanifomarr@users.noreply.github.com>
2023-02-08 15:57:48 +0800

    Reset function

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#winningScoreSelect')
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = null;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function(){
-    if(!isGameOver){
+    if(!isGameOver && winningScore != null){
         p1Score += 1;
         if(p1Score === winningScore){
-        isGameOver = true;
+            p1Display.classList.add('win')
+            p2Display.classList.add('lose')
+            isGameOver = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -20,12 +24,31 @@ p1Button.addEventListener('click', function(){
 })
 
 p2Button.addEventListener('click', function(){
-    if(!isGameOver){
+    if(!isGameOver && winningScore != null){
         p2Score += 1;
         if(p2Score === winningScore){
-        isGameOver = true;
+            p2Display.classList.add('win')
+            p1Display.classList.add('lose')
+            isGameOver = true;
         }
         p2Display.textContent = p2Score;
     }
     
 })
+
+resetButton.addEventListener('click', reset)
+
+winningScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+function reset(){
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    isGameOver = false;
+    p2Display.classList.remove('win', 'lose')
+    p1Display.classList.remove('win', 'lose')
+}
